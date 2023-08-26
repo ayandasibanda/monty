d2f5fcc888ba86b1edaae76241797e38b0272c3b
ayandasibanda <ayanda.sibanda17@gmail.com>
2023-08-26 10:57:59 +0000

    Adding files

---
+++ nop.c
@@ -0,0 +1,13 @@
+#include "monty.h"
+/**
+  *f_nop- Nothing.
+  *@head: The stack head
+  *@counter: line_number
+  *Return: Nothing
+ */
+void f_nop(stack_t **head, unsigned int counter)
+{
+	(void) counter;
+	(void) head;
+}
+
---
+++ stack.c
@@ -0,0 +1,14 @@
+#include "monty.h"
+/**
+ * f_stack - Prints the top
+ * @head: stack head
+ * @counter: line_number
+ * Return: Nothing.
+*/
+void f_stack(stack_t **head, unsigned int counter)
+{
+	(void)head;
+	(void)counter;
+	bus.lifi = 0;
+}
+
